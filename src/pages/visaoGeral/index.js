ca47942ee5d1e089c75f81391b42e465d7800c88
LaisaAlb <128616494+LaisaAlb@users.noreply.github.com>
2025-05-08 09:48:06 -0300

    Criando o dashboard da visaoGeral

--- src/pages/visaoGeral/index.js
+++ src/pages/visaoGeral/index.js
@@ -1,9 +1,49 @@
+import { useEffect, useRef } from "react";
 import { MdDashboard } from "react-icons/md";
+import Cards from "../../components/cards";
 import NavBar from "../../components/navBar";
 import Title from "../../components/title";
-import Cards from "../../components/cards";
+
+import { Chart } from "chart.js/auto"; //
 
 export default function VisaoGearl() {
+  const nomes = ["Pagos", "Pendentes", "Baixados"];
+  const valores = ["R$ 5000", "R$ 1000", "R$ 2000"];
+
+  const chartRef = useRef(null);
+  const chartInstance = useRef(null);
+
+  useEffect(() => {
+    if (chartInstance.current) {
+      chartInstance.current.destroy();
+    }
+
+    const data = {
+      labels: nomes,
+      datasets: [
+        {
+          data: valores.map((val) =>
+            parseFloat(val.replace("R$ ", "").replace(",", "."))
+          ),
+          backgroundColor: ["#12A405", "#F61717", "#D25903"],
+          hoverBackgroundColor: ["#69DE5E", "#EA4F4F", "#D3722E"],
+        },
+      ],
+    };
+
+    const ctx = chartRef.current.getContext("2d");
+    chartInstance.current = new Chart(ctx, {
+      type: "pie",
+      data: data,
+    });
+
+    return () => {
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+      }
+    };
+  }, [nomes, valores]);
+
   return (
     <div>
       <NavBar />
@@ -13,8 +53,8 @@ export default function VisaoGearl() {
         </Title>
 
         <Cards
-          nomes={["Recebido", "Pendente", "Agendado"]}
-          valores={["R$ 10.000", "R$ 2.500", "R$ 3.000"]}
+          nomes={nomes}
+          valores={valores}
           informacoes={[
             "Recebido até hoje",
             "Boletos em aberto",
@@ -22,7 +62,13 @@ export default function VisaoGearl() {
           ]}
         />
 
-
+        {/* Gráfico de pizza com canvas */}
+        <div
+          className="grafico"
+          style={{ width: "25%", marginTop: "90px", marginLeft: "150px" }}
+        >
+          <canvas ref={chartRef} width="400" height="400"></canvas>
+        </div>
       </div>
     </div>
   );
